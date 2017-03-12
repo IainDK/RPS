c2b067cf8c71c2486d7f781bd0096afb624be0b0
Iain Kinnish <iainkinnish@googlemail.com>
2017-03-12 15:47:56 +0000

    Setting-up Heroku.

--- app/models/user.rb
+++ app/models/user.rb
@@ -11,6 +11,6 @@ class User
 
 end
 
-DataMapper.setup(:default, "postgres://localhost/rock_paper_scissors_#{ENV['RACK_ENV']}")
+DataMapper.setup(:default, ENV['DATABASE_URL'] || "postgres://localhost/bookmark_manager_#{ENV['RACK_ENV']}")
 DataMapper.finalize
 DataMapper.auto_upgrade!
